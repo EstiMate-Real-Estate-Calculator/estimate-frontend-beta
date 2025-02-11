ffa09e1d4451ea6e45d3acf28479f29ebee9c847
Jonathan Baptiste <59654985+Joeybaptiste11@users.noreply.github.com>
2025-02-11 10:46:38 -0500

    config

--- postcss.config.js
+++ postcss.config.js
@@ -1,8 +1,7 @@
 module.exports = {
   plugins: {
-    'tailwindcss/nesting': {},
+    'postcss-nesting': {}, // Use postcss-nesting instead of tailwindcss/nesting
     tailwindcss: {},
-    '@tailwindcss/postcss': {},
     autoprefixer: {},
   },
 };
