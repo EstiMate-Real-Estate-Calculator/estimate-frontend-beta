7c063a1d45e1aab9c5cd11b8c21a5d33431d3575
Jonathan Baptiste <59654985+Joeybaptiste11@users.noreply.github.com>
2025-02-11 09:33:53 -0500

    tail

--- next.config.mjs
+++ next.config.mjs
@@ -1,8 +1,14 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
+  swcMinify: true, // Enable faster builds with SWC minification
+
   images: {
     domains: ['ap.rdcpix.com', 'photos.zillowstatic.com'],
+  },
+
+  experimental: {
+    runtime: 'nodejs', // Forces Node.js runtime (Fix for jsonwebtoken issues)
   },
 };
 
